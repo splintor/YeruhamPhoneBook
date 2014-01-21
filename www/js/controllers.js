a4a64dd94a16072dd5135472ea77bdfe86a1b9c2
Shmulik Flint <splintor@gmail.com>
2014-01-21 20:17:59 +0200

    Improve sorting of results

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,6 @@
 angular.module('myApp.controllers', [])
     .controller('MainCtrl', ['$scope', '$rootScope', '$window', '$location', function ($scope, $rootScope, $window, $location) {
         $scope.slide = '';
-        $scope.orderProp = 'title';
         $rootScope.back = function() {
           $scope.slide = 'slide-right';
           $window.history.back();
@@ -15,6 +14,21 @@ angular.module('myApp.controllers', [])
     }])
     .controller('PageListCtrl', ['$scope', 'Page', function ($scope, Page) {
         $scope.pages = Page.query();
+        var pad = "00000"
+        $scope.padNum = function(n) {
+            var s = '' + n;
+            return pad.substring(0, pad.length - s.length) + s;
+        }
+        $scope.orderFunc = function(page) { 
+            var search = $scope.$rootScope.search;
+            if(typeof search != 'string' || search == '') return page.title;
+            search = search.toLowerCase();
+            var titleIndex =  page.title.toLowerCase().indexOf(search); 
+            if(titleIndex > -1) return "A" + $scope.padNum(titleIndex) + page.title;
+            var textIndex =  page.text.toLowerCase().indexOf(search); 
+            if(textIndex > -1) return "B" + $scope.padNum(textIndex) + page.title;
+            return "C" + page.title;
+        }
     }])
     .controller('PageDetailCtrl', ['$scope', '$routeParams', 'Page', function ($scope, $routeParams, Page) {
         $scope.page = Page.get({name: $routeParams.pageName});
