60abcd019c8a404a756388a771d4b083350f0e60
Shmulik Flint <splintor@gmail.com>
2014-01-28 07:56:18 +0200

    Small indentation fix

--- www/js/data-services.js
+++ www/js/data-services.js
@@ -4,18 +4,18 @@
 
     var pages = currentData.pages,
 
-        findByName = function (name) {
-            var page = null,
-                l = pages.length,
-                i;
-            for (i = 0; i < l; i = i + 1) {
-                if (pages[i].name === name) {
-                    page = pages[i];
-                    break;
-                }
+    findByName = function (name) {
+        var page = null,
+            l = pages.length,
+            i;
+        for (i = 0; i < l; i = i + 1) {
+            if (pages[i].name === name) {
+                page = pages[i];
+                break;
             }
-            return page;
-        };
+        }
+        return page;
+    };
 
     angular.module('myApp.dataServices', [])
         .factory('PageTable', [
