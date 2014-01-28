cd17e121e2cc8c6161324b9b59019d1b62d342bd
Shmulik Flint <splintor@gmail.com>
2014-01-28 08:54:55 +0200

    whitespace

--- www/js/app.js
+++ www/js/app.js
@@ -16,14 +16,14 @@ config(['$routeProvider', function ($routeProvider) {
 }]).
 filter("pagesFilter", function($filter) {
     return function(pages, search) {
-    	if(typeof search != 'string' || search == '') return [];
-    	var parseToWords = function (value) {
-	        var pos = value.indexOf('"');
-	        if (pos == -1) return value.split(" ");
-	        var nextPos = value.indexOf('"', pos + 1);
-	        if (nextPos == -1) return parseToWords(value.replace(/"/, ""));
-	        return parseToWords(value.substr(0, pos)).concat(value.substr(pos + 1, nextPos - pos - 1)).concat(parseToWords(value.substr(nextPos + 1)));
-	    };
+        if(typeof search != 'string' || search == '') return [];
+        var parseToWords = function (value) {
+            var pos = value.indexOf('"');
+            if (pos == -1) return value.split(" ");
+            var nextPos = value.indexOf('"', pos + 1);
+            if (nextPos == -1) return parseToWords(value.replace(/"/, ""));
+            return parseToWords(value.substr(0, pos)).concat(value.substr(pos + 1, nextPos - pos - 1)).concat(parseToWords(value.substr(nextPos + 1)));
+        };
         var searchWords = parseToWords(search.toLowerCase());
         var matchWord = function (page, word) {
             return page.title.toLowerCase().indexOf(word) > -1 ||
@@ -34,6 +34,6 @@ filter("pagesFilter", function($filter) {
             return searchWords.length > 0 && searchWords.every(function(word) { return matchWord(page, word); });
         };
         var result = $filter('filter')(pages, searchFunc);
-    	return result.length < 20 ? result : [];
+        return result.length < 20 ? result : [];
     };
 });
