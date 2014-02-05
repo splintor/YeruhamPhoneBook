87a118bf51d6bd199d4d7d2c0cd60ecd357936d1
Shmulik Flint <splintor@gmail.com>
2014-02-05 09:56:49 +0200

    android assets

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -7,23 +7,23 @@ angular.module('myApp', [
     'ngSanitize',
     'myApp.controllers',
     'myApp.dataServices'
-]).
-config(['$routeProvider', function ($routeProvider) {
+])
+.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/pages', {templateUrl: 'partials/page-list.html', controller: 'PageListCtrl'});
     $routeProvider.when('/pages/:pageName', {templateUrl: 'partials/page-detail.html', controller: 'PageDetailCtrl'});
     $routeProvider.otherwise({redirectTo: '/pages'});
     //$routeProvider.otherwise({redirectTo: '/welcome'});
-}]).
-filter("pagesFilter", function($filter) {
+}])
+.filter("pagesFilter", function($filter) {
     return function(pages, search) {
-    	if(typeof search != 'string' || search == '') return [];
-    	var parseToWords = function (value) {
-	        var pos = value.indexOf('"');
-	        if (pos == -1) return value.split(" ");
-	        var nextPos = value.indexOf('"', pos + 1);
-	        if (nextPos == -1) return parseToWords(value.replace(/"/, ""));
-	        return parseToWords(value.substr(0, pos)).concat(value.substr(pos + 1, nextPos - pos - 1)).concat(parseToWords(value.substr(nextPos + 1)));
-	    };
+        if(typeof search != 'string' || search == '') return [];
+        var parseToWords = function (value) {
+            var pos = value.indexOf('"');
+            if (pos == -1) return value.split(" ");
+            var nextPos = value.indexOf('"', pos + 1);
+            if (nextPos == -1) return parseToWords(value.replace(/"/, ""));
+            return parseToWords(value.substr(0, pos)).concat(value.substr(pos + 1, nextPos - pos - 1)).concat(parseToWords(value.substr(nextPos + 1)));
+        };
         var searchWords = parseToWords(search.toLowerCase());
         var matchWord = function (page, word) {
             return page.title.toLowerCase().indexOf(word) > -1 ||
@@ -34,6 +34,18 @@ filter("pagesFilter", function($filter) {
             return searchWords.length > 0 && searchWords.every(function(word) { return matchWord(page, word); });
         };
         var result = $filter('filter')(pages, searchFunc);
-    	return result.length < 20 ? result : [];
+        return result.length < 20 ? result : [];
     };
-});
+})
+.directive('focusOn', function () {
+    return {
+        link: function (scope, element, attrs) {
+            scope.$watch(attrs.focusOn, function (value) {
+                if (value === true) {
+                    element[0].focus();
+                    scope[attrs.focusOn] = false;
+                }
+            });
+        }
+    };
+});;
