9a16bc13366dedb92989957c03f4eabd2b48f7cb
Shmulik Flint <splintor@gmail.com>
2014-07-28 21:38:08 +0300

    Missing DataExtractor files

---
+++ DataExtractor/js/DataExtractorApp.js
@@ -0,0 +1,86 @@
+﻿'use strict';
+
+angular.module('myApp', [
+    'ngTouch',
+    'ngRoute',
+    'ngAnimate',
+    'ngSanitize',
+    'myApp.controllers'
+])
+// .config(['$routeProvider', function ($routeProvider) {
+//     $routeProvider.when('/pages', {templateUrl: 'partials/page-list.html', controller: 'PageListCtrl'});
+//     $routeProvider.when('/pages/:pageName', {templateUrl: 'partials/page-detail.html', controller: 'PageDetailCtrl'});
+//     $routeProvider.otherwise({redirectTo: '/pages'});
+// }])
+.filter("pagesFilter", function ($filter) {
+    return function (pages, $scope) {
+        try {
+            var titleFilter = $scope.titleFilter;
+            var textFilter = $scope.textFilter;
+            var urlFilter = $scope.urlFilter;
+            var nameFilter = $scope.nameFilter;
+            var htmlFilter = $scope.htmlFilter;
+            var parseToWords = function(value) {
+                var pos = value.indexOf('"');
+                if (pos == -1) {
+                    var words = value.split(" ");
+                    for (var i = 0; i < words.length; ++i) {
+                        var match = words[i].match(/^\#\#(.*)/);
+                        if (match) words.splice(i, 1, new RegExp(match[1]));
+                    }
+
+                    return words;
+                }
+
+                var nextPos = value.indexOf('"', pos + 1);
+                if (nextPos == -1) return parseToWords(value.replace(/"/, ""));
+                return parseToWords(value.substr(0, pos)).concat(value.substr(pos + 1, nextPos - pos - 1)).concat(parseToWords(value.substr(nextPos + 1)));
+            };
+            var matchFunc = function(s, filter) {
+                return filter == undefined || filter.length == 0 || s.toLowerCase().indexOf(filter) > -1;
+            }
+            var searchFunc = function (page) {
+                return matchFunc(page.title, titleFilter) &&
+                    matchFunc(page.text, textFilter) &&
+                    matchFunc(page.url, urlFilter) &&
+                    matchFunc(page.name, nameFilter) &&
+                    matchFunc(page.html, htmlFilter);
+            };
+            var results = $filter('filter')(pages, searchFunc);
+            $scope.resultsCount = results.length;
+            return results;
+        } catch (exception) {
+            console.log("An exception has occured in pagesFilter: " + exception);
+            return [];
+        }
+    };
+})
+.filter("mailFilter", function ($filter) {
+    return function (text, $scope) {
+        try {
+            var result = text.match(/(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim);
+            if (!result) return result;
+            return '<a href="mailto:' + result + '">' + "abc" + '</a>';
+        } catch (exception) {
+            console.log("An exception has occured in pagesFilter: " + exception);
+            return [];
+        }
+    };
+})
+.directive('focusOn', function () {
+    return {
+        link: function (scope, element, attrs) { 
+            scope.$watch(attrs.focusOn, function (value) {
+                try {
+                    if (value === true) {
+                        element[0].focus();
+                        scope.showKeyboard();
+                        scope[attrs.focusOn] = false;
+                    }
+                } catch (exception) {
+                    console.log("An exception has occured in focusOn directive: " + exception);
+                }
+            });
+        }
+    };
+});;
---
+++ DataExtractor/js/DataExtractorControllers.js
@@ -0,0 +1,59 @@
+﻿'use strict';
+
+angular.module('myApp.controllers', [])
+    .controller('MainCtrl', [
+        '$scope', '$rootScope', '$window', '$location', function($scope, $rootScope, $window, $location) {
+            try {
+                $rootScope.getMail = function (text) {
+                    try {
+                        var result = text.match(/(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim);
+                        if (!result) return result;
+                        return '<a href="mailto:' + result[0] + '">' + result[0] + '</a>';
+                    } catch (exception) {
+                        console.log("An exception has occured in pagesFilter: " + exception);
+                        return 'aaa';
+                    }
+                }
+
+                $rootScope.getPhone = function (text, index) {
+                    try {
+                        var result = text.match(/(\b[0-9][0-9\-\_\.]{5,11}\b)/gim);
+                        return result ? result[index] : null;
+                    } catch (exception) {
+                        console.log("An exception has occured in pagesFilter: " + exception);
+                        return 'aaa';
+                    }
+                }
+
+                $scope.pages = dataExtractorData.pages;
+                $rootScope.linkify = function(t, onlyNumbers) {
+                    // taken from http://stackoverflow.com/a/3890175/46635
+
+                    if (!onlyNumbers) {
+                        //URLs starting with http://, https://, or ftp://
+                        var replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+                        t = t.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+                        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+                        var replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+                        t = t.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+                        //Change email addresses to mailto: links.
+                        var replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+                        t = t.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+                    }
+
+                    //Change phone numbers to tel:: links.
+                    var replacePattern4 = /(\b[0-9][0-9\-\_\.]{5,11}\b)/gim;
+                    t = t.replace(replacePattern4, '<a href="tel:$1">$1</a>');
+
+                    return t;
+                };
+                $scope.orderFunc = function(p) {
+                    return p.title;
+                };
+            } catch (exception) {
+                console.log("An exception has occured in PageDetailCtrl: " + exception);
+            }
+        }
+    ]);
