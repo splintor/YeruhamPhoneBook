c9b81aa199e2130b8b1ec72387e0c1e9761690b1
Shmulik Flint <splintor@gmail.com>
2014-11-09 00:46:15 +0200

    Various updates - copied to android

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -1,114 +1,131 @@
-'use strict';
+﻿'use strict';
 
 angular.module('myApp.controllers', [])
     .controller('MainCtrl', [
         '$scope', '$rootScope', '$window', '$location', function($scope, $rootScope, $window, $location) {
-            $scope.slide = '';
-            $rootScope.back = function() {
-                //$scope.slide = 'slide-left';
-                $window.history.back();
-            };
-            $rootScope.go = function(path, currentSearch, $event) {
-                if ($event && $event.target && $event.target.href) return; // ignore if a link was clicked
-                $rootScope.rememberedSearch = currentSearch;
-                //$scope.slide = 'slide-right';
-                $location.url(path);
-            };
-            $rootScope.linkify = function(t, onlyNumbers) {
-                // taken from http://stackoverflow.com/a/3890175/46635
+            try {
+                $scope.slide = '';
+                $rootScope.back = function() {
+                    //$scope.slide = 'slide-left';
+                    $window.history.back();
+                };
+                $rootScope.go = function(path, currentSearch, $event) {
+                    if ($event && $event.target && $event.target.href) return; // ignore if a link was clicked
+                    $rootScope.rememberedSearch = currentSearch;
+                    //$scope.slide = 'slide-right';
+                    $location.url(path);
+                };
+                $rootScope.linkify = function(t, onlyNumbers) {
+                    // taken from http://stackoverflow.com/a/3890175/46635
 
-                if (!onlyNumbers) {
-                    //URLs starting with http://, https://, or ftp://
-                    var replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-                    t = t.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+                    if (!onlyNumbers) {
+                        //URLs starting with http://, https://, or ftp://
+                        var replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+                        t = t.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
 
-                    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-                    var replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-                    t = t.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+                        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+                        var replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+                        t = t.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
 
-                    //Change email addresses to mailto: links.
-                    var replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-                    t = t.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
-                }
+                        //Change email addresses to mailto: links.
+                        var replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+                        t = t.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+                    }
 
-                //Change phone numbers to tel:: links.
-                var replacePattern4 = /(\b[0-9][0-9\-\_\.]{5,11}\b)/gim;
-                t = t.replace(replacePattern4, '<a href="tel:$1">$1</a>');
+                    //Change phone numbers to tel:: links.
+                    var replacePattern4 = /(\b[0-9][0-9\-\_\.]{5,11}\b)/gim;
+                    t = t.replace(replacePattern4, '<a href="tel:$1">$1</a>');
 
-                return t;
-            };
+                    return t;
+                };
+            } catch (exception) {
+                console.log("An exception has occured in PageDetailCtrl: " + exception);
+            }
         }
     ])
     .controller('PageListCtrl', [
-        '$scope', '$rootScope', 'PageTable', '$timeout', function ($scope, $rootScope, pageTable, $timeout) {
-            $scope.pages = pageTable.getAllPages();
-            $scope.search = $rootScope.rememberedSearch;
-            $scope.linkify = $rootScope.linkify;
-            pageTable.onPagesUpdate = function (updatedPagesCount) {
-                console.log("updatedPagesCount: " + updatedPagesCount);
-                if (updatedPagesCount == null) { // database was loaded, and refresh is needed
-                    $scope.pages = pageTable.getAllPages();
-                } else { // only count was updated
-                    $scope.updatedPagesCount = updatedPagesCount;
+        '$scope', '$rootScope', 'PageTable', '$timeout', function($scope, $rootScope, pageTable, $timeout) {
+            try {
+                $scope.pages = pageTable.getAllPages();
+                $scope.search = $rootScope.rememberedSearch;
+                $scope.linkify = $rootScope.linkify;
+                pageTable.onPagesUpdate = function(updatedPagesCount, updatedPages) {
+                    console.log("updatedPagesCount: " + updatedPagesCount);
+                    if (updatedPagesCount == null) { // database was loaded, and refresh is needed
+                        $scope.pages = pageTable.getAllPages();
+                    } else { // only count was updated
+                        $scope.updatedPages = updatedPages;
+                        $scope.updatedPagesCount = updatedPagesCount;
+                    }
+                    if (!$scope.$$phase) {
+                        console.log("Calling $scope.$apply()");
+                        $scope.$apply(); // we might need to apply, as this update can occur within a transaction callback, to which PhoneGap is not aware
+                    }
                 }
-                if (!$scope.$$phase) {
-                    console.log("Calling $scope.$apply()");
-                    $scope.$apply(); // we might need to apply, as this update can occur within a transaction callback, to which PhoneGap is not aware
+
+                $scope.forceUpdate = function() {
+                    console.log("forceUpdate");
+                    $timeout(function() { $scope.searchFocus = true; }, 0);
+                    pageTable.forceUpdate();
                 }
-            }
 
-            $scope.forceUpdate = function () {
-                console.log("forceUpdate");
-                $timeout(function () { $scope.searchFocus = true; }, 0);
-                pageTable.forceUpdate();
-            }
-
-            $scope.resetUpdatesPagesCount = function() { $scope.updatedPagesCount = 0; }
-
-            // resultsOverflow can be one of the following values:
-            //   -2 means waiting for timeout to update
-            //   -1 means there was no overflow
-            //   0 means search was not found
-            //   Positive number means the overflow number of results
-            $scope.resultsOverflow = -1;
-            $scope.$timeout = $timeout;
-            var pad = "00000";
-            var padNum = function(n) {
-                var s = '' + n;
-                return pad.substring(0, pad.length - s.length) + s;
-            };
-            $scope.trimText = function(t, n) {
-                return t.length > n ? t.substring(0, n) + "..." : t;
-            };
-            $scope.orderFunc = function(p) {
-                var search = $scope.search;
-                if (typeof search != 'string' || search == '') return p.title;
-                search = search.toLowerCase();
-                var titleIndex = p.title.toLowerCase().indexOf(search);
-                if (titleIndex > -1) return "A" + padNum(titleIndex) + p.title;
-                var textIndex = p.text.toLowerCase().indexOf(search);
-                if (textIndex > -1) return "B" + padNum(textIndex) + p.title;
-                return "C" + p.title;
-            };
-            $scope.clearSearch = function() {
-                $scope.search = '';
-                $timeout(function() { $scope.searchFocus = true; }, 0);
-                $timeout(function() { $scope.searchFocus = true; }, 200);
-                $timeout(function() { $scope.searchFocus = true; }, 400);
-            };
-            $scope.showKeyboard = function() {
-                if (plugins && plugins.softKeyboard) {
-                    plugins.softKeyboard.show(function() {}, function(errDescr) { $scope.search = "Error occured: " + errDescr; });
+                $scope.resetUpdatesPagesCount = function() { $scope.updatedPagesCount = 0; }
+                $scope.openUpdatesPages = function() {
+                    resetUpdatesPagesCount();
+                    $scope.search = "#חדשים";
                 }
-            };
+
+                // resultsOverflow can be one of the following values:
+                //   -2 means waiting for timeout to update
+                //   -1 means there was no overflow
+                //   0 means search was not found
+                //   Positive number means the overflow number of results
+                $scope.resultsOverflow = -1;
+                $scope.$timeout = $timeout;
+                var pad = "00000";
+                var padNum = function(n) {
+                    var s = '' + n;
+                    return pad.substring(0, pad.length - s.length) + s;
+                };
+                $scope.trimText = function(t, n) {
+                    return t.length > n ? t.substring(0, n) + "..." : t;
+                };
+                $scope.orderFunc = function(p) {
+                    var search = $scope.search;
+                    if (typeof search != 'string' || search == '') return p.title;
+                    search = search.toLowerCase();
+                    var titleIndex = p.title.toLowerCase().indexOf(search);
+                    if (titleIndex > -1) return "A" + padNum(titleIndex) + p.title;
+                    var textIndex = p.text.toLowerCase().indexOf(search);
+                    if (textIndex > -1) return "B" + padNum(textIndex) + p.title;
+                    return "C" + p.title;
+                };
+                $scope.clearSearch = function() {
+                    $scope.search = '';
+                    $timeout(function() { $scope.searchFocus = true; }, 0);
+                    $timeout(function() { $scope.searchFocus = true; }, 200);
+                    $timeout(function() { $scope.searchFocus = true; }, 400);
+                };
+                $scope.showKeyboard = function() {
+                    if (plugins && plugins.softKeyboard) {
+                        plugins.softKeyboard.show(function() {}, function(errDescr) { $scope.search = "Error occured: " + errDescr; });
+                    }
+                };
 
 // ReSharper disable once Html.EventNotResolved
-            document.addEventListener("deviceready", $scope.showKeyboard, false);
+                document.addEventListener("deviceready", $scope.showKeyboard, false);
+            } catch (exception) {
+                console.log("An exception has occured in PageListCtrl: " + exception);
+            }
         }
     ])
     .controller('PageDetailCtrl', [
         '$scope', '$routeParams', '$rootScope', 'PageTable', function($scope, $routeParams, $rootScope, pageTable) {
-            $scope.page = pageTable.getPage($routeParams.pageName);
-            $scope.linkify = $rootScope.linkify;
+            try {
+                $scope.page = pageTable.getPage($routeParams.pageName);
+                $scope.linkify = $rootScope.linkify;
+            } catch (exception) {
+                console.log("An exception has occured in PageDetailCtrl: " + exception);
+            }
         }
     ]);
--- platforms/android/assets/www/js/data-services.js
+++ platforms/android/assets/www/js/data-services.js
@@ -1,4 +1,4 @@
-'use strict';
+﻿'use strict';
 
 (function () {
     var logInfo = function(s) { console.log(s); }
@@ -10,19 +10,28 @@
     var pageTable = {
         getAllPages: function() { return currentData.pages; },
         getPage: function(name) {
-            for (var i = 0; i < currentData.pages.length; ++i) {
-                var p = currentData.pages[i];
-                if (p.name === name) {
-                    return p;
+            try {
+                for (var i = 0; i < currentData.pages.length; ++i) {
+                    var p = currentData.pages[i];
+                    if (p.name === name) {
+                        return p;
+                    }
                 }
+            } catch (exception) {
+                console.log("An exception has occured in getPage: " + exception);
             }
             return null;
         },
         onPagesUpdate: null,
     }
 
-    var raiseOnPagesUpdate = function(updatedPagesCount) {
-        if (pageTable.onPagesUpdate) pageTable.onPagesUpdate(updatedPagesCount);
+    var raiseOnPagesUpdate = function(updatedPagesCount, updatedPages) {
+        try {
+            currentData.updatedPages = updatedPages || [];
+            if (pageTable.onPagesUpdate) pageTable.onPagesUpdate(updatedPagesCount, updatedPages);
+        } catch (exception) {
+            console.log("An exception has occured in raiseOnPagesUpdate: " + exception);
+        }
     }
 
     var onDatabaseTransactionError = function (tx, error) {
@@ -74,26 +83,50 @@
                     });
                 }
                 currentData.pages = pages;
+                window.aboutPage = getAboutPage();
+                currentData.pages.push(aboutPage);
                 logInfo("currentData.pages.length: " + currentData.pages.length);
                 raiseOnPagesUpdate(null); // force list refresh
             });
         }, onDatabaseTransactionError, callback);
     };
 
-    window.processDatabaseStartup = function() {
-        var deferred = window.deferService.defer();
-        logInfo("In processDatabaseStartup");
-        var database = window.openDatabase("YeruhamPhoneBookDB", "1.0", "Yeruham Phonebook Database", 500000);
-        var autoUpdateCallback = function() { tryToUpdateDatabase(database, false); };
-        if (getLastUpdateDate()) {
-            loadDatabase(database, autoUpdateCallback);
-        } else {
-            initializeDatabase(database, autoUpdateCallback);
+    var getAboutPage = function() {
+        var text = 'נכתב ע"י שמוליק פלינט (splintor@gmail.com). \n' +
+            'גרסה 2.\n' +
+            currentData.pages.length + ' דפים';
+        if (currentData.updatedPages && currentData.updatedPages.length) {
+            text += ', ' + currentData.updatedPages.length + ' חדשים';
         }
+        text += '.\n';
+        return {
+            name: "about-page",
+            title: "אפליקצית ספר הטלפונים של ירוחם",
+            text: text,
+            html: text.replace(/\n/g, '<br>'),
+            dummyPage: true,
+        };
+    }
 
-        pageTable.forceUpdate = function () { tryToUpdateDatabase(database, true); };
+    window.processDatabaseStartup = function() {
+        try {
+            var deferred = window.deferService.defer();
+            logInfo("In processDatabaseStartup");
+            var database = window.openDatabase("YeruhamPhoneBookDB", "1.0", "Yeruham Phonebook Database", 500000);
+            var autoUpdateCallback = function() { tryToUpdateDatabase(database, false); };
+            if (getLastUpdateDate()) {
+                loadDatabase(database, autoUpdateCallback);
+            } else {
+                initializeDatabase(database, autoUpdateCallback);
+            }
 
-        return deferred.promise;
+            pageTable.forceUpdate = function() { tryToUpdateDatabase(database, true); };
+
+            return deferred.promise;
+        } catch (exception) {
+            console.log("An exception has occured in processDatabaseStartup: " + exception);
+            return null;
+        }
     }
 
     var onDeviceReady = function () {
@@ -105,28 +138,34 @@
         processDatabaseStartup();
     };
 
+    window.aboutPage = getAboutPage();
+    currentData.pages.push(aboutPage);
+
     // ReSharper disable once Html.EventNotResolved
     document.addEventListener("deviceready", onDeviceReady, false);
 
-    var tryToUpdateDatabase = function (database, forceUpdate) {
-        var connectionType = navigator.network ? navigator.network.connection.type : undefined;
-        logInfo("connection type is " + connectionType);
-        if (!forceUpdate && connectionType != Connection.WIFI && connectionType != Connection.ETHERNET) {
-            logInfo("Auto-update only occurs on fast connections (wi-fi or ethernet).");
-            raiseOnPagesUpdate(null); // Make sure the list is refreshed after the database was loaded.
-            return;
-        }
-        var updateURL = "https://script.google.com/macros/s/AKfycbwk3WW_pyJyJugmrj5ZN61382UabkclrJNxXzEsTDKrkD_vtEc/exec?UpdatedAfter=";
+    var tryToUpdateDatabase = function(database, forceUpdate) {
         try {
+            var connectionType = navigator.network ? navigator.network.connection.type : undefined;
+            logInfo("connection type is " + connectionType);
+// ReSharper disable UseOfImplicitGlobalInFunctionScope
+            if (!forceUpdate && connectionType != Connection.WIFI && connectionType != Connection.ETHERNET) {
+// ReSharper restore UseOfImplicitGlobalInFunctionScope
+                logInfo("Auto-update only occurs on fast connections (wi-fi or ethernet).");
+                raiseOnPagesUpdate(null); // Make sure the list is refreshed after the database was loaded.
+                return;
+            }
+
+            var updateURL = "https://script.google.com/macros/s/AKfycbwk3WW_pyJyJugmrj5ZN61382UabkclrJNxXzEsTDKrkD_vtEc/exec?UpdatedAfter=";
             var url = updateURL + new Date(Number(getLastUpdateDate())).toISOString();
             raiseOnPagesUpdate(-1); // loading...
             logInfo("getting updates from " + url);
-            window.httpService.get(url).success(function (data) {
+            window.httpService.get(url).success(function(data) {
                 try {
                     logInfo("Updated page count: " + data.pages.length);
-                    database.transaction(function (tx) {
+                    database.transaction(function(tx) {
                         try {
-                            data.pages.forEach(function (page) {
+                            data.pages.forEach(function(page) {
                                 tx.executeSql("DELETE FROM pages WHERE url=?", [page.url]);
                                 if (!page.isDeleted) addPageToDatabase(tx, page);
                             });
@@ -134,7 +173,7 @@
                             saveLastUpdateDate(data.maxDate);
                             var updatedPagesCount = data.pages.length;
                             if (forceUpdate && updatedPagesCount == 0) updatedPagesCount = -2; // mark as not found
-                            raiseOnPagesUpdate(updatedPagesCount);
+                            raiseOnPagesUpdate(updatedPagesCount, data.pages);
                         } catch (updateEx) {
                             logError("Failed to update data. error: " + updateEx);
                             raiseOnPagesUpdate(0); // clear "loading..." message
@@ -144,7 +183,7 @@
                     logError("Failed to update database: " + e);
                     raiseOnPagesUpdate(0); // clear "loading..." message
                 };
-            }).error(function (data, status) {
+            }).error(function(data, status) {
                 logError("http get failed.\nstatus: " + data + ",\nstatus: " + status);
                 raiseOnPagesUpdate(0); // clear "loading..." message
             });
@@ -157,12 +196,17 @@
     angular.module('myApp.dataServices', [])
         .factory('PageTable', [
             '$q', '$http', function($q, $http) {
-                window.httpService = $http;
-                window.deferService = $q;
+                try {
+                    window.httpService = $http;
+                    window.deferService = $q;
 
-                if (window.deviceReady) processDatabaseStartup();
+                    if (window.deviceReady) processDatabaseStartup();
 
-                return pageTable;
+                    return pageTable;
+                } catch (exception) {
+                    console.log("An exception has occured in PageDetailCtrl: " + exception);
+                    return null;
+                }
             }
         ]);
 }());
