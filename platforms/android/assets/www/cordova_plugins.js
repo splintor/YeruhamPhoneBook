0e24a5faeeb758b77dfb3122550136146c8a6340
Shmulik Flint <splintor@gmail.com>
2015-08-17 19:08:20 +0300

    Add cordoba-plugin-whitelist to enable open links
    http://stackoverflow.com/questions/26546566/mobile-phone-call-or-mailto-doesnt-work-at-all
    
    http://devgirl.org/2015/04/24/apache-cordova-newsflash/

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -14,13 +14,19 @@ module.exports = [
         "clobbers": [
             "Connection"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "net.same2u.phonegap.plugin.SoftKeyboard": "0.0.1",
-    "org.apache.cordova.network-information": "0.2.10-dev"
+    "org.apache.cordova.network-information": "0.2.10-dev",
+    "cordova-plugin-whitelist": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -3,7 +3,8 @@ module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "net.same2u.phonegap.plugin.SoftKeyboard": "0.0.1"
+    "net.same2u.phonegap.plugin.SoftKeyboard": "0.0.1",
+    "cordova-plugin-whitelist": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
