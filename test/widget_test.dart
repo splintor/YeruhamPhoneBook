a00e931481619aad4813245ee157587f71466ba3
Shmulik Flint <shmulikf@wix.com>
2019-12-07 23:17:10 +0200

    Fix test warning

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:yeruham_phone_book/main.dart';
+import 'package:yeruhamphonebook/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
