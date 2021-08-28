58a15f244accdae3ddf109bfc2d7ef7daa9b4a40
Shmulik Flint <shmulikf@wix.com>
2021-08-29 01:02:09 +0300

    Use new site color instead of plain purple

--- lib/main.dart
+++ lib/main.dart
@@ -66,12 +66,28 @@ class Page {
 bool isPageSearchable(Page page) => page.isDeleted != true && page.text.isNotEmpty;
 
 class YeruhamPhonebookApp extends StatelessWidget {
+  static const int _primaryColor = 0xFF5F1B68;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: appTitle,
-      theme: ThemeData(primarySwatch: Colors.purple),
+      theme: ThemeData(primarySwatch: const MaterialColor(
+        _primaryColor,
+        <int, Color>{
+          50: Color(0xFFF3E5F5),
+          100: Color(0xFFE1BEE7),
+          200: Color(0xFFCE93D8),
+          300: Color(0xFFBA68C8),
+          400: Color(0xFFAB47BC),
+          500: Color(_primaryColor),
+          600: Color(0xFF8E24AA),
+          700: Color(0xFF7B1FA2),
+          800: Color(0xFF6A1B9A),
+          900: Color(0xFF4A148C),
+        },
+      )),
       home: const Main(),
       localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
         GlobalMaterialLocalizations.delegate,
