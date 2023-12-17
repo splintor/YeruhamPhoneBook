7a0681b605f636032ac8c4450c074d69f75703d4
Shmulik Flint <shmulikf@wix.com>
2023-12-17 07:43:24 +0200

    null-safety - fix httpsCertificates.dart

--- lib/httpsCertificates.dart
+++ lib/httpsCertificates.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 // https://stackoverflow.com/a/61312927/46635
 class AcceptAllHttpOverrides extends HttpOverrides{
   @override
-  HttpClient createHttpClient(SecurityContext context){
+  HttpClient createHttpClient(SecurityContext? context){
     return super.createHttpClient(context)
       ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
   }
